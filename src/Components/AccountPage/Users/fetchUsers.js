c36ff262c832646ebacf23951440a5571acb2a85
Karol Gutkowski <01171298@pw.edu.pl>
2024-02-05 10:57:25 +0100

    fetching users count from backend

--- src/Components/AccountPage/Users/fetchUsers.js
+++ src/Components/AccountPage/Users/fetchUsers.js
@@ -1,14 +1,27 @@
 import { api_address } from "../../../api_addres";
+import { getLoggedInUser } from "../../LoginLogic/loginLogic";
 
 export const fetchUsersCount = async () =>
 {
     try {
-        const response = await fetch(`${api_address}/users`);
+        const user = getLoggedInUser();
+
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", `Bearer ${user.token}`);
+
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+          };
+
+        const response = await fetch(`${api_address}/admin/customer_count`, requestOptions);
+
+
         if (!response.ok) {
             throw new Error('Failed to fetch users');
         }
-        const data = await response.json();
-        return data.length;
+        const users_count = await response.json();
+        return users_count;
     } catch (error) {
         console.error('Error fetching users:', error);
         return 0;
